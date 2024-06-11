3f4f876cca1390fd5420dca0ad48d14c2b838638
vpotukuchi <122627601+vpotukuchi@users.noreply.github.com>
2024-06-11 14:16:47 +0100

    updated known mac ids and added mac recognition.

--- wearable_alert/node.ino
+++ wearable_alert/node.ino
@@ -24,3 +24,36 @@ Details Configs = {
     D1,
     false
 };
+
+// List of known Access-Point MAC addresses
+const char* knownMACAddresses[] = {
+    "00:11:22:33:44:55",
+    "66:77:88:99:AA:BB",
+    "CC:DD:EE:FF:00:11"
+};
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server
+WiFiClient client;
+// Setup the MQTT client class by passing in the WiFi client and MQTT server details
+Adafruit_MQTT_Client mqtt(&client, "io.adafruit.com", 1883, Configs.adafruitUsername, Configs.adafruitKey);
+// Setup a feed to publish to
+Adafruit_MQTT_Publish wifiNameFeed = Adafruit_MQTT_Publish(&mqtt, (String(Configs.adafruitUsername) + "/feeds/" + Configs.feedName).c_str());
+
+void connectToWiFi(const Details& wifiDetails) {
+    Serial.println("Connecting to WiFi...");
+    WiFi.begin(wifiDetails.ssid, wifiDetails.password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.print(".");
+    }
+    Serial.println("Connected to WiFi");
+}
+
+bool isKnownMACAddress(const String& macAddress) {
+    for (const char* knownMAC : knownMACAddresses) {
+        if (macAddress.equalsIgnoreCase(knownMAC)) {
+            return true;
+        }
+    }
+    return false;
+}
